d5006af9701818072b18d28da406eab5698c73a2
aoife-fey <93552084+aoife-fey@users.noreply.github.com>
2023-02-08 22:58:21 -0700

    Playing around with RE2

--- examples/indev.cpp
+++ examples/indev.cpp
@@ -1,5 +1,17 @@
 #include "fmt/format.h"
+#include "re2/re2.h"
+#include <cassert>
+#include <string>
 
 int main(int, char *[]) {
-  fmt::println("Hello, myco!");
+  std::string s = "hello how are you";
+
+  RE2 re(R"((\w+))");
+  assert(re.ok());
+
+  auto sp = re2::StringPiece(s);
+  std::string w;
+  while (RE2::FindAndConsume(&sp, re, &w)) {
+    fmt::println("{}", w);
+  }
 }
