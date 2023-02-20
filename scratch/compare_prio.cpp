259c055ca804431aa82114c2f652ac14fa691ff2
aoife-fey <93552084+aoife-fey@users.noreply.github.com>
2023-02-20 14:37:41 -0700

    added v1 back to comparison; removed accidentally added segmented_array

--- scratch/compare_prio.cpp
+++ scratch/compare_prio.cpp
@@ -45,7 +45,7 @@ int main(int, char *[]) {
 //
 //  fmt::print("{}\n", p.debug_stringify());
 
-  std::vector<std::tuple<std::size_t, double, double>> times{};
+  std::vector<std::tuple<std::size_t, double, double, double>> times{};
 
   for (auto N = 100; N <= 5'000; N += 100) {
     std::vector<std::string> items{};
@@ -71,10 +71,11 @@ int main(int, char *[]) {
     for (const auto &[i, e]: myco::enumerate(items))
       check_string += fmt::format("{}{}", i != 0 ? ", " : "", e);
     check_string += "]";
+    myco::seed(0);
     myco::shuffle(items);
 
     auto sw1 = myco::Stopwatch();
-    auto p1 = PrioListV2<int>();
+    auto p1 = PrioListV1<int>();
     for (const auto &i: items)
       p1.add(i, 0, deps_s[i]);
     if (p1.debug_stringify() != check_string)
@@ -82,29 +83,38 @@ int main(int, char *[]) {
     sw1.stop();
 
     auto sw2 = myco::Stopwatch();
-    auto p2 = PrioListV3<int>();
+    auto p2 = PrioListV2<int>();
     for (const auto &i: items)
       p2.add(i, 0, deps_s[i]);
     if (p2.debug_stringify() != check_string)
-      MYCO_LOG_ERROR("Bad {} {}", p2.debug_stringify(), check_string);
+      MYCO_LOG_ERROR("Bad {} {}", p1.debug_stringify(), check_string);
     sw2.stop();
 
-    fmt::print("{:<10} {:<10.3f} {:<10.3f}\n", N, sw1.elapsed_sec(), sw2.elapsed_sec());
+    auto sw3 = myco::Stopwatch();
+    auto p3 = PrioListV3<int>();
+    for (const auto &i: items)
+      p3.add(i, 0, deps_s[i]);
+    if (p3.debug_stringify() != check_string)
+      MYCO_LOG_ERROR("Bad {} {}", p2.debug_stringify(), check_string);
+    sw3.stop();
+
+    fmt::print("{:<10} {:<10.3f} {:<10.3f} {:<10.3f}\n",
+               N, sw1.elapsed_sec(), sw2.elapsed_sec(), sw3.elapsed_sec());
     std::fflush(stdout);
 
-    times.emplace_back(N, sw1.elapsed_sec(), sw2.elapsed_sec());
+    times.emplace_back(N, sw1.elapsed_sec(), sw2.elapsed_sec(), sw3.elapsed_sec());
   }
 
   auto d = rapidcsv::Document("", rapidcsv::LabelParams(-1, -1));
-  d.SetRow<std::string>(0, {"Elements", "V2", "V3"});
+  d.SetRow<std::string>(0, {"Elements", "V1", "V2", "V3"});
   for (std::size_t i = 0; i < times.size(); ++i) {
     d.SetCell(0, i + 1, std::get<0>(times[i]));
     d.SetCell(1, i + 1, std::get<1>(times[i]));
     d.SetCell(2, i + 1, std::get<2>(times[i]));
-//    d.SetCell(3, i+1, std::get<3>(times[i]));
+    d.SetCell(3, i + 1, std::get<3>(times[i]));
   }
   d.Save((HERE / "out.csv").string());
 
   std::filesystem::current_path(HERE);
-  std::system("py compare.py 2");
+  std::system("py compare.py 3");
 }
