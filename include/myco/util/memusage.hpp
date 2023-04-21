1a8d33e61617b04a3509414d12860f25fe46ffda
aoife-fey <93552084+aoife-fey@users.noreply.github.com>
2023-04-21 14:43:39 -0700

    remove everything else

--- include/myco/util/memusage.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace myco {
-
-double memusage_mb();
-
-} // namespace myco
--- src/myco/core/modules/module.cpp
+++
@@ -1,6 +0,0 @@
-#include "myco/core/modules/module.hpp"
-#include "myco/core/engine.hpp"
-
-namespace myco {
-
-} // namespace myco
