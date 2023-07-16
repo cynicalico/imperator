f77415526437a81be34412eb1b33c60178b75912
bogicide <93552084+bogicide@users.noreply.github.com>
2023-07-15 20:48:52 -0700

    fog demo

--- example/indev.cpp
+++ example/indev.cpp
@@ -5,23 +5,48 @@ const auto HERE = std::filesystem::path(__FILE__).parent_path();
 
 class Indev : public baphy::Application {
 public:
+  double ox{0};
+  double oy{0};
   double z{0};
+
+  double x_dt{0.001};
+  double y_dt{0.001};
+  double z_dt{0.001};
+
+  int s{8};
+  int octaves{8};
+  double persistence{0.7};
 
   void initialize() override {}
 
   void update(double dt) override {
     if (input->pressed("escape")) window->set_should_close(true);
 
-    z += dt / 2;
+    z += dt * z_dt;
+    ox += dt * x_dt;
+    oy += dt * y_dt;
   }
 
   void draw() override {
     gfx->clear(baphy::rgb("black"));
 
-    double s = 4;
+    dear->begin("Noise parameters", nullptr, ImGuiWindowFlags_AlwaysAutoResize), [&] {
+      ImGui::InputDouble("x_dt", &x_dt, 0.0, 0.0, "%.6f", ImGuiInputTextFlags_EnterReturnsTrue);
+      ImGui::InputDouble("y_dt", &y_dt, 0.0, 0.0, "%.6f", ImGuiInputTextFlags_EnterReturnsTrue);
+      ImGui::InputDouble("z_dt", &z_dt, 0.0, 0.0, "%.6f", ImGuiInputTextFlags_EnterReturnsTrue);
+      ImGui::InputInt("s", &s);
+      ImGui::InputInt("octaves", &octaves);
+      ImGui::InputDouble("persistence", &persistence);
+    };
+
     for (std::size_t x = 0; x < window->w(); x += s)
       for (std::size_t y = 0; y < window->h(); y += s) {
-        double v = baphy::opensimplex::octave3d_improve_xy((x + (s / 2)) / (double)window->w(), (y + (s / 2)) / (double)window->h(), z, 3, 0.2);
+        double v = baphy::opensimplex::octave3d_improve_xy(
+            ox + ((x + (s / 2.0)) / (double)window->w()),
+            oy + ((y + (s / 2.0)) / (double)window->h()),
+            z,
+            octaves, persistence
+        );
         primitives->square(x, y, s, baphy::rgb(v * 255, v * 255, v * 255));
       }
   }
